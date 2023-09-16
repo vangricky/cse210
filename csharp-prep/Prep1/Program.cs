1785f4040d4a3eb64b4cb58ce67f3f91fc55807b
Ricky Vang <vangcheemeng@yahoo.com>
2023-09-16 15:17:13 -0600

    prep1 variables completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine(firstName);
+        Console.WriteLine(lastName);
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
