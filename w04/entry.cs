5ced28ad85803d3d156521c53477bba95ef4ad26
Ricky Vang <vangcheemeng@yahoo.com>
2023-10-03 13:42:42 -0600

    w04 team activity

---
+++ w04/entry.cs
@@ -0,0 +1,17 @@
+public class Entry
+{
+    public string Content;
+    public string Location;
+    public string Mood;
+    public Entry();
+
+    public void AddEntry()
+    {
+        Console.WriteLine($"{Content}")
+    }
+
+    public void RemoveEntry()
+    {
+
+    }
+}
