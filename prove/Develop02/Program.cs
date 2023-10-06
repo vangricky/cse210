df63bab482a60c72a21ccebc1f33c144c69efeb5
Ricky Vang <vangcheemeng@yahoo.com>
2023-10-05 19:57:20 -0600

    c# w4

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Collections.Generic;
 
 class Program
 {
@@ -18,24 +21,83 @@ class Program
 
         // if user picks option to add a new entry
         // 1. Pick a random prompt from list above.
+        Random random = new Random();
+        int randomIndex = random.Next(0, prompts.Count);
+
+        string selectedPrompt = prompts[randomIndex];
+
         // 2. Display the random selected prompt.
+        Console.WriteLine($"Journal Prompt: {selectedPrompt}");
         // 3. Save response from user
         // 4. Create Response Object
+        Console.Write("Your Entry: ");
+        string Entry = Console.ReadLine();
+
         // 5. Save Response object to Journal Object
 
         Entry entry = new Entry();
         entry.EntryDate = DateTime.Now.ToShortDateString();
-        entry.Prompt = ""; // from prompt above 
-        entry.Response = ""; // pull from user's response
+        entry.Prompt = selectedPrompt; // from prompt above 
+        entry.Response = Entry; // pull from user's response
 
         Journal journal = new Journal();
         journal.JournalName = ""; // ask user for name
         journal.Entries = new List<Entry>();
         journal.Entries.Add(entry);
 
-        // save jorunal to file system
+
+
+        List<Entry> entries = new List<Entry>();
+        entries.Add(entry);
+
+        foreach (Entry e in entries)
+        {
+            Console.WriteLine(e.Response);
+        }
+
+        SaveToFile(entries);
 
 
 
+        List<Entry> newEntries = ReadFromFile();
+        foreach (Entry e in newEntries)
+        {
+            Console.WriteLine(e.Response);
+        }
+
+    }
+
+
+    // save jorunal to file system
+
+    public static void SaveToFile(List<Entry> entries)
+    {
+        Console.WriteLine("Saving to file...");
+
+        string filename = "entries.txt";
+
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry e in entries)
+            {
+                outputFile.WriteLine($"Today's Date: {e.EntryDate}. Today's Prompt: {e.Prompt} Your Response: {e.Response}");
+            }
+        }
+    }
+
+    public static List<Entry> ReadFromFile()
+    {
+        Console.WriteLine("Reading list from file...");
+        List<Entry> entries = new List<Entry>();
+        string filename = "entries.txt";
+
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        return entries;
     }
 }
