cfbf1325dc4b19379df5ba0928905bee4f88aa8c
Ricky Vang <vangcheemeng@yahoo.com>
2023-10-10 23:33:51 -0600

    cs

---
+++ friends.cs
@@ -0,0 +1,12 @@
+public class Friend
+{
+    public string FriendFirstName;
+    public string FriendLastName;
+
+    public Friend()
+
+    public void sayFriendsName()
+    {
+        Console.WriteLine($"{FriendFirstName} {FriendLastname}")
+    }
+}
